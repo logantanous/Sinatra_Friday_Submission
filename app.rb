92c9f98aada9908e41a677cd49d102c002c09c9b
logan tanous and mariah shipley <logan@>
2018-02-14 10:11:28 -0800

    made adding to list work

--- app.rb
+++ app.rb
@@ -1,12 +1,19 @@
 require('sinatra')
 require('sinatra/reloader')
 also_reload('lib/**.*.rb')
+require('./lib/favorite_things')
 require('pry')
 
 get('/') do
+  @list = Item.all()
   erb(:input)
 end
 
-get('/output') do
-  erb(:output)
+post('/') do
+  name = params["name"]
+  item = Item.new(name)
+  item.save()
+  @list = Item.all()
+
+  erb(:input)
 end
